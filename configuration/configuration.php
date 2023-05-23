a86deff700f0bb8e09a6581f4fbefb81c341eec3
rameem2003 <rameem2019@gmail.com>
2023-05-23 09:02:32 +0600

    update

--- configuration/configuration.php
+++ configuration/configuration.php
@@ -1,5 +1,5 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "", "todo_app");
+$conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "b34a07a3095491", "c08da71c", "heroku_3945fee2b3dcb74");
 
 ?>
